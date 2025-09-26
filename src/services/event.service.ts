8766fe5bab73b25f39a05b48f88c84d83304a8ae
Daniel24356 <igwedaniel128@gmail.com>
2025-09-26 07:57:11 -0700

    impl createEvent service

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,5 +1,6 @@
 import { orderBy } from "lodash";
 import UtilsService from "./utils.service";
+import { CreateEventDTO } from "../dtos/event.dto";
 
 
 export default class EventService {
@@ -11,5 +12,9 @@ export default class EventService {
         })
     }
 
-    
+    public async createEvent(data: CreateEventDTO){
+        return await this.utils.dbService.event.create({
+            data,
+        })
+    }
 }
