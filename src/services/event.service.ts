557d7f82501cee6a08f20df5aeafab068800958d
Daniel24356 <igwedaniel128@gmail.com>
2025-09-26 07:54:48 -0700

    impl getAllEvents service

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,8 +1,15 @@
+import { orderBy } from "lodash";
 import UtilsService from "./utils.service";
 
 
 export default class EventService {
     private utils: UtilsService = new UtilsService()
     
+    public async getAllEvents(){
+        return await this.utils.dbService.event.findMany({
+            orderBy: {createdAt: "desc"}
+        })
+    }
+
     
 }
